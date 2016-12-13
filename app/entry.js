eb06f81c8daf84c38bd08dc34304aa4c0f6e9ce9
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-12 19:53:05 -0800

    initial index.js and index.html

--- app/entry.js
+++ app/entry.js
@@ -1,3 +1,16 @@
 'use strict';
 
 require('!!file?name=[name].[ext]!./html/index.html');
+
+let React = require('react');
+let ReactDOM = require('react-DOM');
+
+let Hello = React.createClass({
+  render: function() {
+    return (
+      <div>Hello React Test</div>
+    )
+  }
+});
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
