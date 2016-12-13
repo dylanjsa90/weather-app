aad6969778b0892da218a60fd78f05c449e185a9
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-13 15:59:12 -0800

    had an unsaved change

--- app/entry.js
+++ app/entry.js
@@ -4,22 +4,9 @@ require('./scss/base.scss');
 
 import React from 'react';
 import { render } from 'react-dom'
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Repo from './components/Repo';
-import Repos from './components/Repos';
+import {Router, browserHistory} from 'react-router';
+import routes from '.components/routes'
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-  
-), document.getElementById('app'))
+  <Router routes={routes} history={browserHistory}/>,
+   document.getElementById('app'))
